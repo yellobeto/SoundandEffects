be458dedab34712bb417e779ed4fcf5f45885583
yellobeto <dejesuse1@roverkids.org>
2024-12-12 13:13:57 -0500

    Finished 3.1 Unity

--- SoundandEffects/Assets/Course Library/Scripts/SpawnManager.cs
+++ SoundandEffects/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,16 +5,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
