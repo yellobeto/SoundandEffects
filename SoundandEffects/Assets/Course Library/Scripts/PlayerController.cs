31148e7a578d1b5a4d0f986c6b94ec7de577d737
yellobeto <dejesuse1@roverkids.org>
2024-12-19 13:38:36 -0500

    Unity 3.4

--- SoundandEffects/Assets/Course Library/Scripts/PlayerController.cs
+++ SoundandEffects/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+    private AudioSource playerAudio;
     public float jumpforce = 10;
     public float gravityModifier;
+    public AudioClip jumpSound; 
+    public AudioClip crashSound;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     public bool isOnGround = true;
+    public ParticleSystem dirtParticle;
     public bool gameOver;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
@@ -28,9 +34,11 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             isOnGround = false; 
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
-    
+     
     private void OnCollisionEnter(Collision collision)
     {
        
@@ -38,12 +46,16 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if(collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 
